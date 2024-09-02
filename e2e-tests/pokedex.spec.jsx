0f8d4abacf4fcca64dc5b18d67536e92d432e9c2
TubTubTub <tobykwmok@gmail.com>
2024-09-02 11:38:10 +0800

    fso exercises 11.6 - 11.9

--- e2e-tests/pokedex.spec.jsx
+++ e2e-tests/pokedex.spec.jsx
@@ -4,11 +4,13 @@ describe('Pokedex', () => {
   beforeEach(async ({ page }) => {
     await page.goto('/')
   })
+
   test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  test('pokemon page has proper information', async ({ page}) => {
+
+  test('pokemon page has proper information', async ({ page }) => {
     await page.getByRole('link', { name: 'bulbasaur' }).click()
     await page.getByText('bulbasaur').waitFor()
     await expect(page.getByText('bulbasaur')).toBeVisible()
