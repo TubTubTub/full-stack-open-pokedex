1508736ba417ea411918409577615c8139d39c04
TubTubTub <tobykwmok@gmail.com>
2024-09-02 11:36:40 +0800

    sd

--- e2e-tests/pokedex.spec.jsx
+++ e2e-tests/pokedex.spec.jsx
@@ -1,9 +1,18 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('/')
+  })
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('pokemon page has proper information', async ({ page}) => {
+    await page.getByRole('link', { name: 'bulbasaur' }).click()
+    await page.getByText('bulbasaur').waitFor()
+    await expect(page.getByText('bulbasaur')).toBeVisible()
+    await expect(page.getByText('Overgrow')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:8080/',
-    timeout: 30 * 1000,
+    timeout: 15 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   testDir: './e2e-tests',
