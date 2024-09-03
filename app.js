a3bb62ff157f42fa56ac6f2a82df9f1952fa7bf2
TubTubTub <tobykwmok@gmail.com>
2024-09-03 11:59:50 +0800

    s

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`)
-})
-
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
   res.send('ok')
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
 })
