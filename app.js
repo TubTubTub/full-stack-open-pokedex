5d0e6970686818084dd929051f56bf40745f8462
TubTubTub <tobykwmok@gmail.com>
2024-09-03 12:14:10 +0800

    sdsd

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.status(400).send({ error: 'wtf' })
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
